09b096b9a9d6c3346a6752bc43329edac3d9ecbf
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2025-08-04 13:20:07 +0600

    Generate new AccessToken every 15 minutes

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -115,3 +115,39 @@ export const Logout = async (req, res) => {
     
   }
 };
+
+
+export const RefreshToken = async (req, res) => {
+    try {
+        const refreshToken = req.cookies.refreshToken;
+        if (!refreshToken) {
+            return res.status(401).json({ message: "No refresh token provided" });
+        }
+
+        const decoded = jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET);
+
+        const storedRefreshToken = await redis.get(`refresh_token:${decoded.userId}`);
+
+        if (!storedRefreshToken){
+            return res.status(403).json({ message: "Invalid refresh token" });
+        }
+
+        const accessToken = jwt.sign({ userId: decoded.userId }, process.env.ACCESS_TOKEN_SECRET, {
+            expiresIn: "15m",
+        });
+
+        res.cookie("accessToken", accessToken, {
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production",
+            sameSite: "strict",
+            maxAge: 15 * 60 * 1000, // 15 minutes
+        });
+
+        res.status(200).json({ message: "Token refreshed successfully" });
+        
+    } catch (error) {
+        console.error("Error refreshing token:", error);
+        return res.status(500).json({ message: "Internal server error" });
+        
+    }
+}
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,10 +1,16 @@
-import express from 'express';
-import { Signup, Login, Logout } from '../controller/auth.controller.js';
+import express from "express";
+import {
+  Signup,
+  Login,
+  Logout,
+  RefreshToken,
+} from "../controller/auth.controller.js";
 
 const router = express.Router();
 
-router.post('/signup', Signup);
-router.post('/login', Login);
-router.post('/logout', Logout);
+router.post("/signup", Signup);
+router.post("/login", Login);
+router.post("/logout", Logout);
+router.post("/refresh-token", RefreshToken);
 
 export default router;
