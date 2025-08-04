88f60c83f90f034f4120613adcb3a93f20d28cd0
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2025-08-04 11:44:22 +0600

    Complete Signup with password hashing

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -1,3 +1,19 @@
-export const Signup = (req, res) => {
-    
-}
+import User from "../models/user.model.js";
+
+export const Signup = async (req, res) => {
+  const { name, email, password, role } = req.body;
+
+  const userexists = await User.findOne({ email });
+  if (userexists) {
+    return res.status(400).json({ message: "User already exists" });
+  }
+
+  try {
+    const newUser = new User({ name, email, password, role });
+    await newUser.save();
+    res.status(201).json({ message: "User created successfully" });
+  } catch (error) {
+    console.error("Error creating user:", error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
--- backend/server.js
+++ backend/server.js
@@ -1,14 +1,15 @@
-import express from 'express';
-import authRoutes from './routes/auth.route.js';
-import dotenv from 'dotenv';
-import { connectDB } from './lib/db.js';
-
+import express from "express";
+import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
+const app = express();
+
 const PORT = process.env.PORT || 5000;
 
-const app = express();
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 
