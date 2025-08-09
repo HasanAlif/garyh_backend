c4dd06a806df6bea37752890a9ca2c78dab3cced
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2025-08-09 13:22:22 +0600

    Handle Get & Delete route & controller

--- backend/controller/land.controller.js
+++ backend/controller/land.controller.js
@@ -54,3 +54,65 @@ export const createLand = async (req, res) => {
       .json({ message: "Internal server error: " + error.message });
   }
 };
+
+export const getAllLand = async (req, res) => {
+  try {
+    const lands = await Land.find({});
+
+    res.json({ lands });
+  } catch (error) {
+    res.status(500).json({ message: "Server Error", error });
+  }
+};
+
+
+
+// {This will use after Merge with frontend and can upload images to Cloudinary}
+
+
+
+// export const deleteLand = async (req, res) => {
+//   try {
+//     const land = await Land.findById(req.params.id);
+//     if (!land) {
+//       return res.status(404).json({ message: "Land not found" });
+//     }
+
+//     if (land.image) {
+//       const publicId = land.image.split("/").pop().split(".")[0];
+
+//       try {
+//         await cloudinary.uploader.destroy(`land_images/${publicId}`);
+//         console.log("Image deleted from Cloudinary");
+//       } catch (error) {
+//         res
+//           .status(500)
+//           .json({
+//             message: "Error deleting image from Cloudinary",
+//             error: error.message,
+//           });
+//       }
+
+//       await Land.findByIdAndDelete(req.params.id);
+
+//       res.status(200).json({ message: "Land deleted successfully" });
+//     }
+
+//   } catch (error) {
+//     res.status(500).json({ message: "Server Error", error: error.message });
+//   }
+// };
+
+
+export const deleteLand = async (req, res) => {
+    try {
+        const {id} =req.params;
+        const land = await Land.findByIdAndDelete(id);
+        if (!land) {
+            return res.status(404).json({ message: "Land not found" });
+        }
+        res.status(200).json({ message: "Land deleted successfully" });
+    } catch (error) {
+        res.status(500).json({ message: "Server Error", error: error.message });
+    }
+}
--- backend/routes/land.route.js
+++ backend/routes/land.route.js
@@ -1,9 +1,11 @@
 import express from 'express';
 import { landOwnerRoute, protectRoute } from '../middleware/auth.middleware.js';
-import { createLand } from '../controller/land.controller.js';
+import { createLand, getAllLand, deleteLand } from '../controller/land.controller.js';
 
 const router = express.Router();
 
+router.get('/lands', protectRoute, landOwnerRoute, getAllLand);
 router.post('/addland', protectRoute, landOwnerRoute, createLand);
+router.delete('/deleteland/:id', protectRoute, landOwnerRoute, deleteLand);
 
 export default router;
