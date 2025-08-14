654c40effceed830683a20b544709cb9661f0714
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2025-08-14 11:33:24 +0600

    Updates

--- backend/controller/auth.controller.js
+++ backend/controller/auth.controller.js
@@ -55,14 +55,27 @@ const generateTokenAndSetCookie = (res, userId) => {
     sameSite: "strict",
     maxAge: 7 * 24 * 60 * 60 * 1000, // 7 days
   });
+
+  return accessToken;
 };
 
 export const Signup = async (req, res) => {
   const { name, email, password, role } = req.body;
 
+  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+  if (!email || !emailRegex.test(email)) {
+    return res.status(400).json({ message: "Invalid Email Address" });
+  }
+
+  if (password && password.length < 8) {
+    return res
+      .status(400)
+      .json({ message: "Password must be 8 characters long" });
+  }
+
   const userexists = await User.findOne({ email });
   if (userexists) {
-    return res.status(400).json({ message: "User already exists" });
+    return res.status(400).json({ message: "User Already Exists" });
   }
 
   try {
@@ -100,7 +113,9 @@ export const Signup = async (req, res) => {
     });
   } catch (error) {
     console.error("Error creating user:", error);
-    return res.status(500).json({ message: "Internal server error" });
+    return res
+      .status(500)
+      .json({ message: "Internal server error", error: error.message });
   }
 };
 
@@ -150,16 +165,17 @@ export const Login = async (req, res) => {
       // await storeRefreshToken(user._id, refreshToken);
       // setCookies(res, accessToken, refreshToken);
 
-      generateTokenAndSetCookie(res, user._id);
+      const accessToken = generateTokenAndSetCookie(res, user._id);
 
       res.status(200).json({
+        message: "Login successful",
         user: {
           _id: user._id,
           name: user.name,
           email: user.email,
           role: user.role,
         },
-        message: "Login successful",
+        accessToken,
       });
     } else {
       return res.status(400).json({ message: "Invalid email or password" });
--- backend/server.js
+++ backend/server.js
@@ -1,35 +1,11 @@
-import express from "express";
 import dotenv from "dotenv";
-import { connectDB } from "./lib/db.js";
-import cookieParser from "cookie-parser";
-import authRoutes from "./routes/auth.route.js";
-import landRoutes from "./routes/land.route.js";
-import travelerRoutes from "./routes/traveler.route.js";
-import contactRoutes from "./routes/contact.route.js";
-import bookingRoutes from "./routes/booking.route.js";
-import globalRoutes from "./routes/global.route.js";
-import dashboardRoutes from "./routes/dashboard.route.js";
 import { cancelExpiredBookings } from "./controller/booking.controller.js";
+import app from "./app.js";
+import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
-const app = express();
-
-app.use(express.json({ limit: "10mb" }));
-app.use(express.urlencoded({ extended: true, limit: "10mb" }));
-app.use(cookieParser());
-
 const PORT = process.env.PORT || 5000;
-
-app.use("/api/auth", authRoutes);
-app.use("/api/landowner", landRoutes);
-app.use("/api/lands", landRoutes);
-app.use("/api/traveler", travelerRoutes);
-app.use("/api/contact", contactRoutes);
-app.use("/api/booking", bookingRoutes);
-app.use("/api/global", globalRoutes);
-app.use("/api/dashboard", dashboardRoutes);
-
 app.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   await connectDB();
