1da6598e150e1eb47e5b622971b2f6928961cf50
Mehedi Hasan Alif <alifalmehedihasan@gmail.com>
2025-08-18 12:59:46 +0600

    Implement Socket Server and completed chat engine backend functionality

--- backend/app.js
+++ backend/app.js
@@ -15,10 +15,12 @@ dotenv.config();
 
 const app = express();
 
-app.use(cors({
-  origin: 'http://localhost:5173', 
-  credentials: true
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import dotenv from "dotenv";
 import { cancelExpiredBookings } from "./controller/booking.controller.js";
-import app from "./app.js";
+import { server } from "./lib/socket.js";
 import { connectDB } from "./lib/db.js";
 
 dotenv.config();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, async () => {
+server.listen(PORT, async () => {
   console.log(`Server is running on http://localhost:${PORT}`);
   await connectDB();
   await cancelExpiredBookings();
